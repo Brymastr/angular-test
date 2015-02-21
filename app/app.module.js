e3ebe49c03153bcadba5f44775de877c97d6f36a
Brycen Dorsay <dorsay@live.ca>
2015-02-20 19:34:23 -0800

    Modules
    jQuery, Bootstrap, Normalize

--- app/app.module.js
+++ app/app.module.js
@@ -2,4 +2,4 @@
  * Created by brycen on 15-02-14.
  */
 
-var app = angular.module('app', []);
+var app = angular.module('portfolioApp', []);
--- app/app.routes.js
+++ app/app.routes.js
@@ -1,3 +1,7 @@
 /**
  * Created by Brycen on 2015-02-19.
  */
+
+app.controller('mainController', function() {
+
+});
