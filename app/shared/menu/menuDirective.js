f9d7fc6cb0f3c80f49e6e8a8f618c33dd7712a2d
Brycen Dorsay <dorsay@live.ca>
2015-02-20 23:01:06 -0800

    Menu directive and home component
    Showing and hiding properly based on screen size. sidebar is hidden on small devices. Will eventually replace it with a normal collapsed menu for small screens

--- app/shared/menu/menuDirective.js
+++ app/shared/menu/menuDirective.js
@@ -1,3 +1,11 @@
 /**
  * Created by Brycen on 2015-02-19.
  */
+
+angular.module('portfolioApp')
+    .directive('menuDirective', function() {
+        return {
+            restrict: 'E',
+            templateUrl: '/app/shared/menu/menuView.html'
+        };
+    });
