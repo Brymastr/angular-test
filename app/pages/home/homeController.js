f2e9c0602372cc2306a15fb522d83d3ce98a392f
Brycen Dorsay <dorsay@live.ca>
2015-02-25 13:53:24 -0800

    ng-repeat on home items

--- app/pages/home/homeController.js
+++ app/pages/home/homeController.js
@@ -2,3 +2,26 @@
  * Created by Brycen on 2015-02-19.
  */
 
+angular.module('portfolioApp')
+    .controller('HomeController', function() {
+        this.items = homeItems;
+    });
+
+var homeItems = [
+    {
+        title: 'About',
+        description: 'All about me'
+    }, {
+        title: 'Experience',
+        description: 'Jobs and technologies'
+    }, {
+        title: 'Web Projects',
+        description: 'Sites and apps built for the web'
+    }, {
+        title: 'Mobile Projects',
+        description: 'Android and iOS application projects'
+    }, {
+        title: 'Other Projects',
+        description: 'Because trying new things is fun'
+    }
+];
