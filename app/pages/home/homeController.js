6132aaa450ba44d3c2000d43b74559eb31a6279c
Brycen Dorsay <dorsay@live.ca>
2015-02-25 19:17:02 -0800

    Font size responsive

--- app/pages/home/homeController.js
+++ app/pages/home/homeController.js
@@ -11,22 +11,22 @@ var homeItems = [
     {
         title: 'About',
         description: 'All about me',
-        color: materialColors[0]
+        color: materialColors[4]
     }, {
         title: 'Experience',
         description: 'Jobs and technologies',
-        color: materialColors[1]
+        color: materialColors[5]
     }, {
         title: 'Web Projects',
         description: 'Sites and apps built for the web',
-        color: materialColors[2]
+        color: materialColors[6]
     }, {
         title: 'Mobile Projects',
         description: 'Android and iOS application projects',
-        color: materialColors[3]
+        color: materialColors[7]
     }, {
         title: 'Other Projects',
         description: 'Because trying new things is fun',
-        color: materialColors[4]
+        color: materialColors[8]
     }
 ];
