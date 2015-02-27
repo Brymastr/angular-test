cd7343bacaa1d7f82872381b3b5f81ebe9d088c9
Brycen Dorsay <dorsay@live.ca>
2015-02-26 17:24:36 -0800

    Proper routing

--- app/app.routes.js
+++ app/app.routes.js
@@ -2,10 +2,15 @@
  * Created by Brycen on 2015-02-19.
  */
 
-app.config(function($routeProvider) {
+app.config(function($routeProvider, $locationProvider) {
     $routeProvider
         .when('/', {
             templateUrl: '../app/pages/home/homeView.html',
             controller: 'HomeController'
+        })
+        .when('/about',  {
+            templateUrl: '../app/pages/about/aboutView.html'
         });
+
+    $locationProvider.html5Mode(true);
 });
--- app/pages/home/homeController.js
+++ app/pages/home/homeController.js
@@ -2,31 +2,38 @@
  * Created by Brycen on 2015-02-19.
  */
 
-angular.module('portfolioApp')
-    .controller('HomeController', function() {
-        this.items = homeItems;
-    });
+app.controller('HomeController', function($scope, $location) {
+    this.items = homeItems;
+    $scope.goTo = function(path) {
+        $location.path(path);
+    }
+});
 
 var homeItems = [
     {
         title: 'About',
         description: 'All about me',
+        url: '/about',
         color: materialColors[5]
     }, {
         title: 'Experience',
         description: 'Jobs and technologies',
+        url: '/exp',
         color: materialColors[6]
     }, {
         title: 'Web Projects',
         description: 'Sites and apps built for the web',
+        url: '/web',
         color: materialColors[7]
     }, {
         title: 'Mobile Projects',
         description: 'Android and iOS application projects',
+        url: '/mobile',
         color: materialColors[8]
     }, {
         title: 'Other Projects',
         description: 'Because trying new things is fun',
+        url: '/other',
         color: materialColors[9]
     }
 ];
