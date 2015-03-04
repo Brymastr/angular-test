33fe3cb46201d89be97331b219fac5a73ba48141
Brycen Dorsay <dorsay@live.ca>
2015-03-04 00:00:26 -0800

    Project page content background and shadow

--- app/pages/projects/projectsController.js
+++ app/pages/projects/projectsController.js
@@ -26,49 +26,58 @@ var projects = [
     {
         id: 0,
         name: 'Tasty Ratios',
-        description: 'An Android application I built with a team at BCIT that interfaces with the ReFleX Wireless Inc. bluetooth food scale for creating and following recipes.',
+        shortDescription: 'An Android application I built with a team at BCIT that interfaces with the ReFleX Wireless Inc. bluetooth food scale for creating and following recipes.',
+        longDescription: 'Long Descritpion',
         technologies: ['Android', 'Bluetooth', 'Bootstrap 3', 'Materialize'],
         category: 'mobile',
         color: materialColors[0]
     }, {
         id: 1,
         name: 'ClickRacer',
-        description: 'A website I built as a game that counts how many times you can click the mouse in 10 seconds.',
+        shortDescription: 'A website I built as a game that counts how many times you can click the mouse in 10 seconds.',
+        longDescription: 'Long Description',
         technologies: ['PHP', 'Laravel 4', 'Bootstrap 3', 'MySql'],
         category: 'web',
+        url: 'http://click-racer.com',
         color: materialColors[1]
     }, {
         id: 2,
         name: 'Randomizr',
-        description: 'A website I built that encapsulates user specified websites into a new link that forwards randomly to one of the specified websites.',
+        shortDescription: 'A website I built that encapsulates user specified websites into a new link that forwards randomly to one of the specified websites.',
+        longDescription: 'Long Description',
         technologies: ['PHP', 'Laravel 4', 'MySql'],
         category: 'web',
+        url: 'http://randomizr.link',
         color: materialColors[2]
     }, {
         id: 3,
         name: 'Convoy',
-        description: 'A mobile platform for tracking the location of users in the context of group travel.',
+        shortDescription: 'A mobile platform for tracking the location of users in the context of group travel.',
+        longDescription: 'Long Description',
         technologies: ['Android', 'Node.js', 'Express.js', 'MongoDB'],
         category: 'mobile',
         color: materialColors[3]
     }, {
         id: 4,
         name: 'StudentConnect',
-        description: 'An iOS 8 / ASP.NET MVC 4 application I am building with a team at BCIT that enables instructors to more easily and directly communicate with students.',
+        shortDescription: 'An iOS 8 / ASP.NET MVC 4 application I am building with a team at BCIT that enables instructors to more easily and directly communicate with students.',
+        longDescription: 'Long Description',
         technologies: ['iOS 8', 'Swift', 'Node.js', 'Express.js', 'ASP.NET MVC 4', 'MySql/MongoDB/SQLite3'],
         category: 'both',
         color: materialColors[4]
     }, {
         id: 5,
         name: 'Eat, Play, Sleep',
-        description: 'A website I am building with a team at BCIT that provides hotel, restaurant, and attraction information from Yelp.com and other rating sources.',
+        shortDescription: 'A website I am building with a team at BCIT that provides hotel, restaurant, and attraction information from Yelp.com and other rating sources.',
+        longDescription: 'Long Description',
         technologies: ['Codeigniter 3', 'Bootstrap 3', 'MySql'],
         category: 'web',
         color: materialColors[5]
     }, {
         id: 6,
         name: 'OverAchiever',
-        description: 'An Android application tracks your location and awards you points when you arrive at a predefined location.',
+        shortDescription: 'An Android application tracks your location and awards you points when you arrive at a predefined location.',
+        longDescription: 'Long Description',
         technologies: ['Android', 'SQLite3'],
         category: 'mobile',
         color: materialColors[6]
