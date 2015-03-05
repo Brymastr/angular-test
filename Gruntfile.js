5ba79e8d1516d8df19a5d705ee5c998f580f8605
Brycen Dorsay <dorsay@live.ca>
2015-03-05 00:21:34 -0800

    Grunt working js minification
    mangle: false

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,31 +12,56 @@ module.exports = function(grunt) {
       }
     },
     concat: {
-      js: {
+      angular: {
+        src: [
+          'bower_components/angular/angular.js',
+          'bower_components/angular-animate/angular-animate.js',
+          'bower_components/angular-route/angular-route.js'
+        ],
+        dest: 'app/angular.min.js'
+      },
+      app: {
         src: [
           'app/app.module.js',
           'app/app.routes.js',
-          'app/**/*.js'
+          'app/pages/**/*.js',
+          'app/shared/**/*.js'
         ],
         dest: 'app/app.min.js'
       }
     },
     uglify: {
-      js: {
+      options: {
+        mangle: false
+      },
+      angular: {
+        src: 'app/angular.min.js',
+        dest: 'app/angular.min.js'
+      },
+      app: {
         src: 'app/app.min.js',
         dest: 'app/app.min.js'
       }
+    },
+    auto_install: {
+      local: {}
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-auto-install');
 
   grunt.registerTask('default', [
-    'concat:js',
-    'uglify:js',
+    'concat:angular',
+    'concat:app',
+    'uglify:angular',
+    'uglify:app',
     'cssmin:target'
   ]);
+
+  grunt.registerTask('install', 'auto_install:local');
+
 
 };
