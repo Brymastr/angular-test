3898ad21f36d10c59939e54e7d449900ac0130ba
Brycen <dorsay@live.ca>
2015-05-12 23:28:27 -0700

    removed deploy script

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-IMAGE="brymastr/portfolio"
-docker ps | grep $IMAGE | awk '{print $1}' | xargs docker stop
-docker pull $IMAGE
-docker run -d -e VIRTUAL_HOST=brycendorsay.com $IMAGE
